def3d3468e8a80fd2674a678e24ac4ee58f1ed12
vanessasilva-cs <vanesilva.cs@gmail.com>
2023-02-01 18:59:49 -0300

    Ajustes

--- js/main.js
+++ js/main.js
@@ -110,6 +110,17 @@
         navigationText: false
     });
 
+		$("#owl-slider-experience").owlCarousel({
+        navigation: false,
+        pagination: true,
+        itemsCustom : [
+	        [0, 1],
+	        [1500, 1],
+	        [1500, 1]
+	     ],
+        navigationText: false
+    });
+
 	/*----------------------------------------------------- */
 	/* Alert Boxes
   	------------------------------------------------------- */
